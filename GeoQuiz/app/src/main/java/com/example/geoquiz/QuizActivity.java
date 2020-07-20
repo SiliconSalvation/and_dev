2780b7e78702b7301d3746404c87ae37922fc402
Kazuma <kazuma@pop-os.localdomain>
2020-07-19 23:07:11 -0400

    finished main portion of chapter 3

--- GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -1,8 +1,8 @@
 package com.example.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
@@ -13,12 +13,15 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button trueButton;
     private Button falseButton;
     private ImageButton nextButton;
     private ImageButton prevButton;
     private TextView questionTextView;
-    private LinearLayout ll;
+    //private LinearLayout ll;
 
     private Question[] questionBank = new Question[] {
             new Question(R.string.question_australia, true),
@@ -34,21 +37,26 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if(savedInstanceState != null) {
+            currentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         questionTextView = (TextView) findViewById(R.id.question_text_view);
 
         /**
-         * Buttons
+         * Button references
          */
         trueButton = (Button) findViewById(R.id.true_button);
         falseButton = (Button) findViewById(R.id.false_button);
         nextButton = (ImageButton) findViewById(R.id.next_button);
         prevButton = (ImageButton) findViewById(R.id.prev_button);
-        ll = (LinearLayout) findViewById(R.id.mainlayout);
+        //ll = (LinearLayout) findViewById(R.id.mainlayout);
 
         /**
-         * Listeners
+         * Listeners for buttons
          */
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,17 +88,69 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
-
-        ll.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                updateIndex(true);
-                updateQuestion();
-            }
-        });
+        //Doesn't work with the new layout
+//        ll.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                updateIndex(true);
+//                updateQuestion();
+//            }
+//        });
 
         updateQuestion();
+    }
+
+    /**
+     * Overrides onStart() and Logs the action
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    /**
+     * Overrides onResume() and Logs the action
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    /**
+     * Overrides onPause() and Logs the action
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSavedInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, currentIndex);
+    }
+
+    /**
+     * Overrides onStop() and Logs the action
+     */
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    /**
+     * Overrides onDestroy and Logs the action
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     /**
@@ -101,14 +161,15 @@ public class QuizActivity extends AppCompatActivity {
      */
     private void updateIndex(boolean nextButtonPressed) {
 
-        if(nextButtonPressed) {
+        if (nextButtonPressed) {
             currentIndex++;
-            currentIndex = currentIndex % questionBank.length;
         } else {
-            if(currentIndex == 0) { currentIndex = questionBank.length; }
+            if(currentIndex == 0) {
+                currentIndex = questionBank.length;
+            }
             currentIndex--;
-            currentIndex = currentIndex % questionBank.length;
         }
+        currentIndex = currentIndex % questionBank.length;
     }
 
     /**
@@ -137,4 +198,5 @@ public class QuizActivity extends AppCompatActivity {
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
+
 }
